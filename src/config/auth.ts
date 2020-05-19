f6e9deaeab754719641d19038dbc7c3f1d483cb0
scard69 <vit.scard@gmail.com>
2020-05-18 21:59:29 -0300

    go barber backend

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '8899394421e040babf4af60434787875',
+    expiresIn: '1d',
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,43 @@
+import 'reflect-metadata';
+
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+import routes from './routes';
+import uploadConfig from './config/upload';
+import AppError from './errors/AppError';
+
+import './database';
+
+const app = express();
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3333',
+//   }),
+// );
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+app.listen(3333, () => {
+  console.log('🚀 server started on port 3333');
+});
